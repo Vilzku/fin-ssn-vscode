53e964808e01f99d1739e3e93bf1c48f9f063e04
Ville Puolitaival <ville.puolitaival@gmail.com>
2025-02-01 21:15:04 +0200

    Add basic tests

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,15 +1,43 @@
 import * as assert from 'assert';
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+import * as sinon from 'sinon';
+import { INVALID_INPUT_ERROR_TEXT } from '../errors';
 
 suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+  let showErrorMessageSpy: sinon.SinonSpy;
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-	});
+  setup(() => {
+    showErrorMessageSpy = sinon.spy(vscode.window, 'showErrorMessage');
+  });
+
+  teardown(() => {
+    showErrorMessageSpy.restore();
+  });
+
+  test('Generate SSN without input', async () => {
+    const document = await vscode.workspace.openTextDocument();
+    await vscode.window.showTextDocument(document);
+    await vscode.commands.executeCommand('fin-ssn.generateSsn');
+    const output = document.getText();
+    assert.strictEqual(output.length, 11);
+  });
+
+  test('Invalid input error message', async () => {
+    const originalText = 'abc';
+    const document = await vscode.workspace.openTextDocument({
+      content: originalText,
+    });
+    const editor = await vscode.window.showTextDocument(document);
+
+    editor.selection = new vscode.Selection(
+      new vscode.Position(0, 0),
+      new vscode.Position(0, 10)
+    );
+    await vscode.commands.executeCommand('fin-ssn.generateSsn');
+
+    assert.ok(
+      showErrorMessageSpy.calledOnceWithExactly(INVALID_INPUT_ERROR_TEXT)
+    );
+    assert.strictEqual(document.getText(), originalText);
+  });
 });
